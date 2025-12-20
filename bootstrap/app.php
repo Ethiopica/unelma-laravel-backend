7dac8494d6d9bb272a6a34b9e696546a26277612
Ethiopica <eliobais@gmail.com>
2025-12-20 23:51:23 +0200

    Fix undefined constant HEADER_X_FORWARDED_ALL

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         // Trust all proxies (required for Railway, Heroku, AWS ELB, etc.)
-        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL);
+        $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'admin' => \App\Http\Middleware\IsAdmin::class,
